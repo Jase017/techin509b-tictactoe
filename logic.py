9201a11e6f1bb59b38f432ce6ac25bc079eeb5d3
Jase017 <148395257+Jase017@users.noreply.github.com>
2023-10-31 15:10:53 -0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,22 +1,47 @@
 # This file is where game logic lives. No input
 # or output happens here. The logic in this file
 # should be unit-testable.
-
-
 def make_empty_board():
-    return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None],
-    ]
+	return [['', '', ''],
+		    ['', '', ''], 
+		    ['', '', '']]
+
+def input_move(board):
+		x = int(input("input row (0-2): "))
+		y = int(input("input col (0-2): "))
+
+		if x < 0 or x > 2 or y < 0 or y > 2:
+			raise ValueError('invalid')
+		if board[x][y] != '':
+			raise ValueError('invalid')
+		return x, y
+
+
+def other_player(now):
+	if now == 'X':
+		next = 'O'
+	else:
+		next = 'X'
+	return next
 
 
 def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-    return None  # FIXME
-
-
-def other_player(player):
-    """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+	for i in range(0, 3):
+		if board[i][0] != '' and board[i][0] == board[i][1] == board[i][2]:
+			print(board[i][0], ' Won')
+			return board[i][0]
+		
+	for i in range(0, 3):
+		if board[0][i] != '' and board[0][i] == board[1][i] == board[2][i]:
+			print(board[0][i], ' Won')
+			return board[0][i]
+		
+	if board[1][1] != '' and (board[0][0] == board[1][1] == board[2][2] or board[2][0] == board[1][1] ==board[0][2]):
+		print(board[1][1], ' Won')
+		return board[1][1]
+	for row in board:
+		for col in row:
+			if col == '':
+				return ''
+	print('Draw')
+	return 'Draw'

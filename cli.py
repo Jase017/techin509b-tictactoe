ba433555357e083a12fe99cc05e6e505a6036936
Jase017 <148395257+Jase017@users.noreply.github.com>
2023-10-31 15:10:22 -0700

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,61 +1,19 @@
 from logic import make_empty_board
-
-
-def input_move(board):
-	try:
-		x = int(input("input row (0-2): "))
-		y = int(input("input col (0-2): "))
-	except:
-		raise ValueError('input invalid, not int')
-	else:
-		if x < 0 or x > 2 or y < 0 or y > 2:
-			raise ValueError('input invalid, should >=0 and <=2')
-		if board[x][y] != '':
-			raise ValueError('input invalid, index already input')
-		return x, y
-
-
-def other_player(now):
-	if now == 'X':
-		next = 'O'
-	else:
-		next = 'X'
-	return next
-
-
-def get_winner(board):
-	for i in range(0, 3):
-		if board[i][0] != '' and board[i][0] == board[i][1] == board[i][2]:
-			print(board[i][0], ' Won')
-			return board[i][0]
-	for i in range(0, 3):
-		if board[0][i] != '' and board[0][i] == board[1][i] == board[2][i]:
-			print(board[0][i], ' Won')
-			return board[0][i]
-	if board[1][1] != '' and (
-			board[0][0] == board[1][1] == board[2][2] or board[2][0] == board[1][1] ==
-			board[0][2]):
-		print(board[1][1], ' Won')
-		return board[1][1]
-	for row in board:
-		for col in row:
-			if col == '':
-				return ''
-	print('Draw')
-	return 'Draw'
-
+from logic import input_move
+from logic import get_winner
+from logic import other_player
 
 if __name__ == '__main__':
     board = make_empty_board()
     winner = ''
     player = 'X'
     for row in board:
-        print(row)
+         print(row)
     while winner == '':
-        print('Next player: ', player)
+        print('Next turn: ', player)
         x, y = input_move(board)
         board[x][y] = player
         for row in board:
-            print(row)
+              print(row)
         winner = get_winner(board)
-        player= other_player(player)
+        player = other_player(player)

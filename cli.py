cc6b960b7caa4c8bfd1a809235f6ae7ad8ab6c63
Jase017 <148395257+Jase017@users.noreply.github.com>
2023-11-06 14:37:55 -0800

    Update cli.py

--- cli.py
+++ cli.py
@@ -3,17 +3,30 @@ from logic import input_move
 from logic import get_winner
 from logic import other_player
 
-if __name__ == '__main__':
+
+def play_game():
     board = make_empty_board()
     winner = ''
     player = 'X'
+
     for row in board:
-         print(row)
+        print(row)
+
     while winner == '':
         print('Next turn: ', player)
         x, y = input_move(board)
+        if x == 'q' and y == 'q':
+            return
         board[x][y] = player
         for row in board:
-              print(row)
+            print(row)
         winner = get_winner(board)
         player = other_player(player)
+
+
+if __name__ == '__main__':
+    while True:
+        play_game()
+        play_again = input('Do you want to play again? (Y/N): ')
+        if play_again.upper() != 'Y':
+            break
